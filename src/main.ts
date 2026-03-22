51c9769ab73acce0cc233dcf582e4b86ad20e2f9
raito <Olanihundaniel0@gmail.com>
2026-03-22 02:31:15 +0100

    fixing auth

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(Number(process.env.PORT ?? 3000));
+  await app.listen(Number(process.env.PORT ?? 3000), '0.0.0.0');
 }
 bootstrap();
